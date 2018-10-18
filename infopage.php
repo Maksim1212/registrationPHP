c67174b0776995fdcc30581329eed7b8b5f6b9b3
Maksim1212 <mr.frost201611@gmail.com>
2018-10-18 09:42:28 +0300

    some fix

--- infopage.php
+++ infopage.php
@@ -1,17 +1,30 @@
 <?php
 session_start();
 require "db.php";
+
 if (isset($_SESSION['logged_user'])) {
+
     // var_dump($_SESSION['logged_user']);
+
     echo "Hello nice to meet you" . PHP_EOL;
 
 
     echo ($_SESSION['logged_user']['name']) . PHP_EOL;
+
     echo ($_SESSION['logged_user']['surname']) . PHP_EOL;
+
     echo ($_SESSION['logged_user'] ['registrationDatedate']) . PHP_EOL;
 
     echo "<a href='logout.php'> Logout </a>";
-} else echo "You are not logged !!!!!";
+
+} else {
+
+    //echo "You are not logged !!!!!";
+    header("Location: index.html");
+    exit;
+
+}
+
 ?>
 <!doctype html>
 <html lang="en">
